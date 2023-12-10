8c8eebca6759e021741deb2fdd90326cfd7ef4d5
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-10 18:08:22 +0500

    Create zain.py

---
+++ zain.py
@@ -0,0 +1,14 @@
+# Importing the necessary modules from Flask
+import flask, render_template
+
+# Creating a Flask web application
+app = flask.Flask(__name__)
+
+# Define a route for the root URL
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Run the app if this script is executed
+if __name__ == '__main__':
+    app.run()
