9cb9b53e79c36de5d2178acfdd77cc7e06ea53d9
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-10 18:19:29 +0500

    Update zain.sh

--- zain.sh
+++ zain.sh
@@ -1,4 +1,4 @@
-venvname="myvenv"
+venvname="zain32"
 
 if [ ! -d "$venvname" ]; then
     python -m venv "$venvname"
@@ -7,5 +7,5 @@ fi
 source "$venvname/bin/activate"
 pip install flask gunicorn
 
-gunicorn -w 4 -b 0.0.0.0:8000 --reload wsgi:app
+gunicorn -w 4 -b 0.0.0.0:3200 --reload zain:app
 
