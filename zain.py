3ad18f4acff74e69efe917f944de6f5787101531
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-10 18:09:03 +0500

    Update zain.py

--- zain.py
+++ zain.py
@@ -7,7 +7,7 @@ app = flask.Flask(__name__)
 # Define a route for the root URL
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('zainaliwaheed.html')
 
 # Run the app if this script is executed
 if __name__ == '__main__':
